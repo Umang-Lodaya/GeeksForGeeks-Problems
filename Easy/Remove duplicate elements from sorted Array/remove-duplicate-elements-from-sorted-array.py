200596be76710bca7efe7aeee3ba1c685e5c308c
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-06-30 23:24:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Remove duplicate elements from sorted Array/remove-duplicate-elements-from-sorted-array.py
@@ -0,0 +1,27 @@
+#User function template for Python
+
+class Solution:	
+	def remove_duplicate(self,a,n):
+	    global A
+		A=list(set(A))
+		A.sort()
+		return len(A)
+
+
+#{ 
+ # Driver Code Starts
+#Initial template for Python
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        N = int(input())
+        A = list(map(int, input().strip().split()))
+        ob = Solution()
+        n = ob.remove_duplicate(A,N)
+        for i in range(n):
+            print(A[i], end=" ")
+        print()
+
+
+# } Driver Code Ends
