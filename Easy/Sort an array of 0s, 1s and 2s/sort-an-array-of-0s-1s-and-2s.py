5094ba6f78fee82d95d7c2e6ee14f7d89ba351a4
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-07-01 10:48:34 +0530

    Updated Solution - GfG to GitHub

--- Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.py
+++ Easy/Sort an array of 0s, 1s and 2s/sort-an-array-of-0s-1s-and-2s.py
@@ -1,27 +1,20 @@
 #User function Template for python3
 
 class Solution:
-    def sort012(self,arr,n):
-        # code here
-        low = mid = 0
-        high = n - 1
-        
+    def sort012(self, arr, n):
+        low = 0; mid = 0; high = n - 1
         while mid <= high:
             if arr[mid] == 0:
-                temp = arr[low]
-                arr[low] = arr[mid]
-                arr[mid] = temp
+                arr[mid], arr[low] = arr[low], arr[mid]
                 low += 1
                 mid += 1
+                
             elif arr[mid] == 1:
                 mid += 1
+            
             else:
-                temp = arr[mid]
-                arr[mid] = arr[high]
-                arr[high] = temp
+                arr[mid], arr[high] = arr[high], arr[mid]
                 high -= 1
-        
-                
 
 
 #{ 
