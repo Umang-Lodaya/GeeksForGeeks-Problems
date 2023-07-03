da4178b4528630f575df6ec7439f8eaf6ad0fdf7
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-07-03 14:55:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Overlapping Intervals/overlapping-intervals.py
@@ -0,0 +1,35 @@
+class Solution:
+	def overlappedInterval(self, arr):
+	    arr.sort()
+	    ans = [arr[0]]
+	    for i in range(1, len(arr)):
+	        if arr[i][0] <= ans[-1][1]:
+	            ans[-1][1] = max(ans[-1][1], arr[i][1])
+	        else:
+	            ans.append(arr[i])
+	    
+	    return ans
+	    
+#{ 
+ # Driver Code Starts
+		
+if __name__ == '__main__':
+    T=int(input())
+    for i in range(T):
+    	n = int(input())
+    	a = list(map(int, input().strip().split()))
+    	Intervals = []
+    	j = 0
+    	for i in range(n):
+    		x = a[j]
+    		j += 1
+    		y = a[j]
+    		j += 1
+    		Intervals.append([x,y])
+    	obj = Solution()
+    	ans = obj.overlappedInterval(Intervals)
+    	for i in ans:
+    		for j in i:
+    			print(j, end = " ")
+    	print()
+# } Driver Code Ends
