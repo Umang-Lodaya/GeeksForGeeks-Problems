c4b59668d2334494e854b63b8265bc3bbc62da9f
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-08-20 19:13:33 +0530

    Updated Solution - GfG to GitHub

--- Medium/Number of occurrence/number-of-occurrence.py
+++ Medium/Number of occurrence/number-of-occurrence.py
@@ -1,41 +1,43 @@
 #User function Template for python3
 class Solution:
-    def find(self, arr, n, x):
-        if x < arr[0] or arr[-1] < x: return -1, -1
-        
-        first = n
-        
-        low = 0; high = n - 1
-        while low <= high:
-            mid = low + (high - low) // 2
-            if arr[mid] == x:
-                first = mid
-                high = mid - 1
-            elif arr[mid] < x:
-                low = mid + 1
-            else:
-                high = mid - 1
-                
-        if first == n: return -1, -1
-        
-        last = 0
-        low = 0; high = n - 1
-        while low <= high:
-            mid = low + (high - low) // 2
-            if arr[mid] == x:
-                last = mid
-                low = mid + 1
-            elif arr[mid] < x:
-                low = mid + 1
-            else:
-                high = mid - 1
-        
-        return first, last
-        
+
 	def count(self,arr, n, x):
-		l, r = self.find(arr, n, x)
-		if l == -1: return 0
-		return r - l + 1
+	    
+		def leftMostEle(arr, n, x):
+		    l = 0; h = n - 1
+		    inx = -1
+		    while l <= h:
+		        mid = l + (h - l) // 2
+		        if arr[mid] == x:
+		            inx = mid
+		            h = mid - 1
+		        elif arr[mid] > x:
+		            h = mid - 1
+		        else:
+		            l = mid + 1
+		    
+		    return inx
+	    
+		def rightMostEle(arr, n, x):
+		    l = 0; h = n - 1
+		    inx = -1
+		    while l <= h:
+		        mid = l + (h - l) // 2
+		        if arr[mid] == x:
+		            inx = mid
+		            l = mid + 1
+		        elif arr[mid] > x:
+		            h = mid - 1
+		        else:
+		            l = mid + 1
+		    
+		    return inx
+		
+		s = leftMostEle(arr, n, x)
+		if s == -1:
+		    return 0
+		e = rightMostEle(arr, n, x)
+		return e - s + 1
 
 #{ 
  # Driver Code Starts
