08849023a99d1c60a2f33c92b26a415b8795709d
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-10-29 17:05:41 +0530

    Updated Solution - GfG to GitHub

--- Easy/DFS of Graph/dfs-of-graph.py
+++ Easy/DFS of Graph/dfs-of-graph.py
@@ -4,20 +4,25 @@ class Solution:
     
     #Function to return a list containing the DFS traversal of the graph.
     def dfsOfGraph(self, V, adj):
-        def dfs(node, graph, visited, component):
-            component.append(node)  # Store answer
-            visited[node] = True  # Mark visited
+        if V == 1:
+            return [0]
         
-            # Traverse to each adjacent node of a node
-            for child in graph[node]:
-                if not visited[child]:  # Check whether the node is visited or not
-                    dfs(child, graph, visited, component)  # Call the dfs recursively
-    
-        node = 0  # Starting node
-        visited = [False]*V  # Make all nodes to False initially
-        component = []
-        dfs(node, adj, visited, component)  # Traverse to each node of a graph
-        return component
+        visited = [False for _ in range(V)]
+        visited[0] = True
+        path = []
+        
+        def dfs(node):
+            nonlocal path
+            visited[node] = True
+            path.append(node)
+            for v in adj[node]:
+                if not visited[v]:
+                    dfs(v)
+        
+        dfs(0)
+        
+        return path
+
 
 #{ 
  # Driver Code Starts
