4ea91b4780547ebc77184d9b3ca6eaff69cc8146
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2023-12-23 11:51:22 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count More than n/k Occurences/count-more-than-nk-occurences.py
+++ Easy/Count More than n/k Occurences/count-more-than-nk-occurences.py
@@ -5,19 +5,17 @@ class Solution:
     
     #Function to find all elements in array that appear more than n/k times.
     def countOccurence(self, arr, n, k):
+        thres = n // k
         freq = {}
         for i in arr:
-            if i not in freq:
-                freq[i] = 0
-            freq[i] += 1
+            freq[i] = freq.get(i, 0) + 1
         
-        ans = 0
-        for i in freq:
-            if freq[i] > (n/k):
-                ans += 1
+        count = 0
+        for k, v in freq.items():
+            if v > thres:
+                count += 1
         
-        return ans
-
+        return count
 
 #{ 
  # Driver Code Starts
