2a59e4b53a3d26dd83b59ca71f1e015e4e32b711
UMANG KIRIT LODAYA <91935807+Umang-Lodaya@users.noreply.github.com>
2024-03-05 17:15:37 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.py
+++ Medium/Maximum Index/maximum-index.py
@@ -1,36 +1,52 @@
 #User function Template for python3
-class Solution:
 
-    def maxIndexDiff(self, arr, n):
-        v_min = [0] * n
-        v_max = [0] * n
-        v_min[0] = arr[0]
-        v_max[n - 1] = arr[n - 1]
+class Solution:
+    def maxIndexDiff(self,a, n): 
+        left = [0]*(n)
+        right = [0]*(n)
+        
+        left[0] = a[0]
+        right[n - 1] = a[n - 1]
         
         for i in range(1, n):
-            v_min[i] = min(v_min[i - 1], arr[i])
-            v_max[n - i - 1] = max(v_max[n - i], arr[n - i - 1])
+            left[i] = min(a[i], left[i - 1])
             
-        ans = 0
-        i = 0
-        j = 0
+        for j in range(n - 2, -1, -1):
+            right[j] = max(a[j], right[j + 1])
+        
+        i = 0; j = 0
+        maxx = -1
+        
         while i < n and j < n:
-            if v_min[i] <= v_max[j]:
-                ans = max(ans, j - i)
+            if right[j] >= left[i]:
+                maxx = max(maxx, j-i)
                 j += 1
             else:
                 i += 1
                 
-        return ans
+        return maxx
+
 
 #{ 
  # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            n=int(input())
+            
+            arr=[int(x) for x in input().strip().split()]
+            ob=Solution()
+            print(ob.maxIndexDiff(arr,n))
+            
+            
+            T-=1
+
+
 if __name__ == "__main__":
-	t = int(input())
-	while(t>0):
-		num = int(input())
-		arr = [int(x) for x in input().strip().split()]
-		ob = Solution()
-		print(ob.maxIndexDiff(arr,num))
-		t-=1
+    main()
 # } Driver Code Ends
